bb72303ebc887d6608773ef787347a6aadd674fe
yu <araera111@gmail.com>
2022-10-20 15:27:18 +0900

    feat:

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
-import { User } from './types/type';
+import { differenceInMilliseconds, subWeeks } from 'date-fns';
+import humanizeDuration, { LanguageCode } from 'humanize-duration';
 
-const main = () => {
-	const user: User = {
-		id: 1,
-		name: 'Takeda'
-	};
-	console.log({ user });
-	console.log('main');
+const isInvalidDate = (date: any) => Number.isNaN(date.getDate());
+
+export const calcWeeksOfPregnancy = (
+	dueDate: Date,
+	today: Date | undefined,
+	languageCode: LanguageCode
+) => {
+	const minus40Date = subWeeks(dueDate, 40);
+	if (isInvalidDate(minus40Date)) return 'invalid date';
+	const resultToday = today === undefined ? new Date() : today;
+	const diff = differenceInMilliseconds(resultToday, minus40Date);
+	return humanizeDuration(diff, {
+		units: ['w', 'd'],
+		round: false,
+		maxDecimalPoints: 0,
+		language: languageCode
+	});
 };
-
-main();
